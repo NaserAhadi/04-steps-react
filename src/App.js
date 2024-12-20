511e1fc104429aae4b0dafedce96ab92e3a62503
Naser Ahadi <n.ahadi@vandar.io>
2024-12-20 17:29:32 +0330

    add state to component and make it reactive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
@@ -5,21 +7,30 @@ const messages = [
 ];
 
 export default function App(){
-    const step=3
+    const [step, setStep] = useState(1);
     const buttonStyle={backgroundColor: '#7950f2', color: 'white'};
 
+    function handlePrevious(){
+        if(step===1) return;
+        setStep(step-1)
+    }
+
+    function handleNext(){
+        if(step===3) return;
+        setStep(step+1)
+    }
     return <div className="steps">
         <div className="numbers">
-            <div className={`${step>=1 && 'active'}`}>1</div>
-            <div className={`${step>=2 && 'active'}`}>2</div>
-            <div className={`${step===3 && 'active'}`}>3</div>
+            <div className={step>=1 && 'active'}>1</div>
+            <div className={step>=2 && 'active'}>2</div>
+            <div className={step===3 && 'active'}>3</div>
         </div>
         <div className="message">
             Step {step}: {messages[step-1]}
         </div>
         <div className="buttons">
-            <button style={buttonStyle}>Previous</button>
-            <button style={buttonStyle}>Next</button>
+            <button style={buttonStyle} onClick={handlePrevious}>Previous</button>
+            <button style={buttonStyle} onClick={handleNext}>Next</button>
         </div>
     </div>
 }
