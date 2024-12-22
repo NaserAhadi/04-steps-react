480550b75daa1c1fbb4b497b9071d878c748988f
Naser Ahadi <n.ahadi@vandar.io>
2024-12-22 14:05:37 +0330

    use functional form as argument of setter state to get the latest state

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ export default function App(){
 
     function handlePrevious(){
         if(step===1) return;
-        setStep(step-1)
+        setStep(s=> s-1)
     }
 
     function handleNext(){
         if(step===3) return;
-        setStep(step+1)
+        setStep(s=> s+1)
     }
     return <>
-        <button className="close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
+        <button className="close" onClick={()=> setIsOpen(is => !is)}>&times;</button>
         { isOpen &&
             <div className="steps">
                 <div className="numbers">
